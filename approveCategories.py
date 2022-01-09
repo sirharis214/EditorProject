34d1f218dc9dd75aba6b9ddafefc43cbd4930e9a
sirharis214 <36185908+sirharis214@users.noreply.github.com>
2022-01-09 13:50:19 -0500

    Add files via upload
    updated functions for better performance and more reliable returns

--- approveCategories.py
+++ approveCategories.py
@@ -1,140 +1,31 @@
-import re # moduel to use regex 
-from datetime import datetime
+import currentListValidation
+"""
+validateCategories() is being called form editor.py
+@param : a dictionary, Key=line#:Value=List of category inputs
+We will return a dictionary with Key=Line#:Value= Dictionary of categoryValid/categoryInvalid:categoryValue
+
+1. Itirate through the dictionary. Confirm if the Category input list is valid. The length must be an even number.
+   If number of categories NOT valid, update dictionary key=Line#:value="Invalid number of categories"
+   If number of categoies IS valid, send the list to have its values validated.
+"""
+
 # first take each line and check the category list length
 # the length of category list must be even, otherwise they are missing a category field
-
+# VV- The function being called from editor.py -VV
 def validateCategories(linesCategoried):
-	def checkMetadata(mainCategoryList):
-		#print("Main Categories are...")
-		#print(mainCategoryList)
-		metadata = {"show_name":"", "show_ID":"", "show_type":"", "show_date":""}
-		metadata.update({"show_name":mainCategoryList[0], "show_ID":mainCategoryList[1], "show_type":mainCategoryList[2], "show_date":mainCategoryList[3]})
-		#print(metadata)
-		# check show_name. Must be 1-100 chars in length
-		if len(metadata["show_name"]) >=1 and len(metadata["show_name"]) <=100:
-			# Valid show_name
-			pass
-		else:
-			# Invalid show_name
-			metadata.update({"show_name":"invalid"})
-
-		# check show ID. first 4 letters A-Z followed by 9 digits
-		pattern = '^[A-Z]{4}[0-9]{9}$'
-		resultID = re.match(pattern, metadata["show_ID"])
-		if resultID:
-			# valid id
-			pass
-		else:
-			#invalid id
-			metadata.update({"show_ID":"invalid"})
-
-		# check show_type. must be either of these options [movie,episode,sports,news]
-		showTypes = ["movie","episode","sports","news"]
-		resultType = metadata["show_type"].lower() in showTypes
-		if resultType:
-			# valid show type
-			#print("Valid show type")
-			pass
-		else:
-			# invalid show type
-			#print("invalid show type")
-			metadata.update({"show_type":"invalid"})
-
-		# check show_date. must be in format "mm-dd-yyyy" 
-		dateFormat = '%m-%d-%Y'
-		resultDate = True
-		try:
-			resultDate = bool(datetime.strptime(metadata["show_date"], dateFormat))
-		except ValueError:
-			resultDate = False
-		if resultDate:
-			# valid date format
-			#print("valid date format")
-			pass
-		else:
-			# invalid date format
-			#print("invalid date format")
-			metadata.update({"show_date":"invalid"})
-		#print(metadata)
-		return metadata
-
-
-	def checkTimecodes(timeCodesList):
-		timecodeFormat = re.compile("^\d{2};\d{2};\d{2};\d{2}$")
-		#print(timeCodesList)
-		#["seg_start", "seg_end"]
-		timescodesDic = {}
-		count = 0
-		for timecode in timeCodesList:
-			if timecodeFormat.match(timecode) is not None:
-				#timecode format matches
-				#print("timecode format matches")
-				#pass
-				if count % 2 == 0:
-					if count == 0:
-						timescodesDic.update({"Timecode IN "+str(count+1):timecode})
-					else:
-						timescodesDic.update({"Timecode IN "+str(count):timecode})
-				else:
-					if count == 1:
-						timescodesDic.update({"Timecode OUT "+str(count):timecode})
-					else:
-						timescodesDic.update({"Timecode OUT "+str(count-1):timecode})
-			else:
-				#timecode format does not match
-				#print("timecode does not match")
-				if count % 2 == 0:
-					if count == 0:
-						timescodesDic.update({"Timecode IN "+str(count+1):"invalid"})
-					else:
-						timescodesDic.update({"Timecode IN "+str(count):"invalid"})
-				else:
-					if count == 1:
-						timescodesDic.update({"Timecode OUT "+str(count):"invalid"})
-					else:
-						timescodesDic.update({"Timecode OUT "+str(count-1):"invalid"})
-			count +=1
-		#print(timeCodesList)
-		return timescodesDic
-
-
-
-	def checkCategoryInputs(currentList):
-		# split the first 4 categories apart, thats metadata. The rest is timecodes
-		main_categories = currentList[:4] # indexs 0 upto-but-not-including 4
-		time_codes = currentList[4:] # index 4 upto-last-index. 
-		metadataDic = checkMetadata(main_categories)
-		timecodeList = checkTimecodes(time_codes)
-		
-		print(metadataDic)
-		print(timecodeList)
-
-	categoryStatus = {}
-	for line in linesCategoried:
-		currentList = linesCategoried[line]
-		listLength = len(currentList)
-
-		if listLength % 2 != 0:
-			categoryStatus.update({line:"Line "+str(line)+" has an invalid number of categories."})
+	categoryStatus = {} # initilize return dictionary.
+	for line in linesCategoried: # itirate through each key in Dictionary (the dict recieved as param)
+		currentList = linesCategoried[line] # the category list of the current key.
+		listLength = len(currentList) # get the length of the list
+		# Check the length 
+		if listLength % 2 != 0: # if the length is not even
+			# category list length is odd, missing a category or not seperated by 2 or more spaces
+			msg = {"Line has an invalid number of categories.":" Seperate each category by 2 or more spaces."}
+			categoryStatus.update({"Line "+str(line):msg})
 		else:
 			#categoryStatus.update({line:"Line "+str(line)+" has valid number of category count."})
-			print("Line "+str(line)+": ")
-			checkCategoryInputs(currentList)
-
+			#print("Line "+str(line)+": ")
+			returnedValidation = currentListValidation.checkCategoryInputs(currentList) # send the current category List to be validated, returns a dict
+			categoryStatus.update({"Line "+str(line):returnedValidation}) # update our return dictionary
 	#print(categoryStatus)
-
-
-# # Each line is split into categories, the category count is a even number, we must validate the entries
-# def validateEntries(current_line):
-#     #print(current_line)
-#     #lineLength = len(current_line)
-#     main_categories = current_line[:4] # index 0 upto but not including 4
-#     time_codes = current_line[4:] # index 4 upto last index
-#     print(main_categories)
-#     print(time_codes)
-#     # Second time verifying timecode start and end pair
-#     if(len(time_codes) % 2) != 0:
-#         # odd length, not complete start and stop time codes
-#         print("Must have pair of timecode start AND timecode end")
-#     else:
-#         print("Pairs of timecards Valid")
+	return categoryStatus
--- makeCategories.py
+++ makeCategories.py
@@ -1,12 +1,17 @@
 import re   # moduel to use regex 
-
-# split the text file per line
+""" This moduel is used in editor.py
+	the function splitLines takes a @param Lines, which is a list all the lines in the file.
+	We will return a dictionary : Key = line# : Value = a list after splitting the line into categories
+	1. Itirate through each line, strip the line of the new line char
+	2. Split the line into categories using regex, each category is seperated by 2 or more blank spaces.
+	3. Once complete, return the dictionary back to editor.py
+"""
 def splitLines(Lines):
 	eachLineCategory = {}  # empty dictionary that will hold each line as key and category list as value
 	count = 0   # start line
 
 	for line in Lines:
-	    count += 1
+	    count += 1 # the current line#
 	    #print("Checking Line # "+str(count)) # line # that we are on
 	    clean_line = line.rstrip("\n") # removing trailing newline char
 	    #print(clean_line)
@@ -14,15 +19,3 @@ def splitLines(Lines):
 	    #print(lineSectioned)
 	    eachLineCategory.update({count:lineSectioned})
 	return eachLineCategory
-	    # # Length of the array once each entry is split
-	    # lineLength = len(lineSectioned)
-	    # # if Length of array is odd, we are missing a category or category are not divided by multispaces
-	    # if(lineLength % 2) == 0:
-	    #     # Even number lengeth, so all categories are entered
-	    #     print("Validating category entries...")
-	    #     validateEntries(lineSectioned)
-	    # else:
-	    #     # Odd number length
-	    #     print("Missing a category or categories are not divided by 2 or more spaces")
-	    #     validateEntries(lineSectioned)
-	    # print("\n")
